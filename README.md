d8298621902a0479a2900de5e036cf802adbcce6
Shankar Vasudevan <s.vasudevan297@gmail.com>
2021-11-17 22:14:27 +0100

    Project setup and build script. Need to get geenrated code compiling

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sudoku Solver
+
+Basic server application that receives Sudoku's as input and generates Sudoku solutions
+
+## Technical design
+
+* Single-threaded event loop that listens to incoming requests
+* Define protocol for client to send requests to server
+** Request: Unsolved sudoku
+** Response: Solved sudoku or Error (if invalid or cannot be solved)
+
+## Technologies used
+
+* C++
+* Protobuf
+* CMake
+* Shell scripting (for builds)
+
+
+
---
+++ build_backend.sh
@@ -0,0 +1,25 @@
+echo "Building SudokuSolverBackend"
+
+# Should check for a specific version of protobuf
+if brew ls --versions protobuf > /dev/null; then
+    echo "Protobuf already installed"
+else
+    echo "Protobuf not installed. Will install now"
+    brew install protobuf
+fi
+
+echo "Generating C++ code from protocol"
+rm -rf backend/generated
+mkdir backend/generated
+protoc --proto_path=proto --cpp_out=backend/generated proto/sudoku_solver.proto
+
+echo "Building"
+cd backend
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
+echo "Finished building"
