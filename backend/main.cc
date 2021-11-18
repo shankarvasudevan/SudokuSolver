998fc23734244ed699681c43b5f50dee2d10c8bf
Shankar Vasudevan <s.vasudevan297@gmail.com>
2021-11-18 22:27:04 +0100

    Cleaned up build script

--- backend/main.cc
+++ backend/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sudoku_solver.pb.h>
 
 int main(int argc, char* argv[])
 {
--- build_backend.sh
+++ build_backend.sh
@@ -1,19 +1,5 @@
 echo "Building SudokuSolverBackend"
 
-# Should check for a specific version of protobuf
-if brew ls --versions protobuf > /dev/null; then
-    echo "Protobuf already installed"
-else
-    echo "Protobuf not installed. Will install now"
-    brew install protobuf
-fi
-
-#echo "Generating C++ code from protocol"
-rm -rf backend/generated
-mkdir backend/generated
-protoc --proto_path=proto --cpp_out=backend/generated proto/sudoku_solver.proto
-
-echo "Building"
 cd backend
 rm -rf build
 mkdir build
