6ebb90f026d3b29c23888586daa0e466cd625b20
Shankar Vasudevan <s.vasudevan297@gmail.com>
2021-12-05 22:12:12 +0100

    Building a basic console TCP client

---
+++ frontend/SudokuClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace SudokuClient
+{
+    class Program
+    {
+        private const int Port = 9999;
+        private const string HostName = "localhost";
+
+        static void Main(string[] args)
+        {
+            using (var client = new TcpClient(HostName, Port))
+            {
+                if (!client.Connected)
+                {
+                    Console.WriteLine("Could not connect");
+                    return;
+                }
+
+                using (NetworkStream stream = client.GetStream())
+                {
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes("Hello server");
+                    stream.Write(data, 0, data.Length);
+                    stream.Close();
+                }
+
+                client.Close();
+            }
+
+            Console.WriteLine("Press ENTER to continue");
+            Console.Read();
+        }
+    }
+}
